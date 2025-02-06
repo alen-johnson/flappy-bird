91334fd1aadc9740a24de7e68e55b4357bbda075
Alen J Ninan <alen.j@captainfresh.in>
2025-02-06 11:35:56 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Flappy Bird with Java
+
+A simple flappy bird game with java
+
+## Tech Stack
+
+    -> Java 17+
---
+++ src/App.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int boardWidth = 360;
+        int boardHeight = 640;
+
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setVisible(true);
+
+        // frame.setLocationRelativeTo(null); 
+
+        frame.setLocation(500, 100);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        FlappyBird flappyBird = new FlappyBird();
+        frame.add(flappyBird);
+        frame.pack();
+        flappyBird.requestFocus();
+        frame.setVisible(true);
+
+    }
+}
